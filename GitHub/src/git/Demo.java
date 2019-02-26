486629ba3623bc4da91f7ac2ed47edc5fa336211
user <user@lenovo>
2019-02-26 21:05:10 +0530

    hiiiiiii hello

---
+++ GitHub/src/git/Demo.java
@@ -0,0 +1,17 @@
+package git;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo {
+	static {
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+	}
+	@Test
+	public void testA() {
+		
+		WebDriver driver =new ChromeDriver();
+		driver.close();
+		}
+}
